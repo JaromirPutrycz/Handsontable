bf45356cda428514387dea73442b17ae4db7b1cc
JaromirPutrycz <jaromirp@wp.pl>
2025-01-15 00:05:51 +0100

    Update zad11Selenium.spec.js

--- zad11Selenium.spec.js
+++ zad11Selenium.spec.js
@@ -1,19 +1,15 @@
 const {By, Builder, Browser} = require('selenium-webdriver');
 const assert = require("assert");
-
 (async function firstTest() {
  let driver;
- 
  try {
  driver = await new Builder().forBrowser(Browser.FIREFOX).build();
  await driver.manage().setTimeouts({implicit: 20000});
  await driver.get('https://handsontable.com/demo')
- 
  const resources = await driver.findElement(By.className('CybotCookiebotDialogBodyButton'));
-
- //console.log(javaButton)
+ console.log(await resources.isDisplayed());  // Check if element is displayed
+ //console.log(Button)
  await resources.click();
-
  } catch (e) {
  console.log(e)
  } finally {
